786a83f3a03f2b6bb914d18c1e1a1adb90cb7a44
AlefeVariani <alefevariani@unochapeco.edu.br>
2014-07-26 22:57:57 -0300

    Exemplo arrays

---
+++ iniciando/arrays.php
@@ -0,0 +1,33 @@
+<?php
+
+$a[0] = "a";
+$a[1] = "b";
+
+echo $a;// Array
+
+$animais = array("Gato", "Cachorro", "Coelho");
+echo $animais[0];// Gato
+
+$nomes = array(0 => "Alefe", 1 => "Joao"); //indices podem ser informados
+echo $nomes[0];
+
+// Array associativos
+$conf = array();
+$conf["nome"] = "Alefe";
+$conf["idade"] = "20";
+$conf["linguagem"] = "PHP";
+
+echo "Nome - " . $conf["nome"] . " Idade - " . $conf["idadde"]; 
+
+$b = array(
+	'times' => array(
+		0 => 'gremio',
+		1 => 'chapecoense',
+		2 => 'boca junior'
+	),
+	'selecoes' => array(
+		'brasil',
+		'alemanha',
+		'argentina'
+	),	
+);
