0c38f24010460bc33cd859130d6f6680ed262517
AlefeVariani <alefevariani@unochapeco.edu.br>
2014-09-24 23:56:24 -0300

    Exemplo de constante

--- _iniciando/exibir_valores.php
+++ _iniciando/exibir_valores.php
@@ -7,8 +7,14 @@
 
 $nome = "Alefe Variani";
 
+// Costante (é indicado definir constante com letra miusculas)
+define('PI', 3.14159265359);
+
 //exibe
-echo $nome; 
+echo $nome;
+
+//exibe constante
+echo PI; 
 
 // alem de exiber, pode retornar um resultado, função do PHP
 print $nome;
