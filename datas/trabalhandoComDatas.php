b2fcf4f8cee1aaf86ac8d9e1d1bb82c2f6539b68
AlefeVariani <alefevariani@unochapeco.edu.br>
2015-03-01 23:16:14 -0300

    novo exemplo com datas

--- datas/trabalhandoComDatas.php
+++ datas/trabalhandoComDatas.php
@@ -3,4 +3,10 @@
 $dataAtual = new DateTime('now');
 
 print_r($dataAtual) . PHP_EOL;
-print_r($dataAtual->format('d/m/Y H:i')) . PHP_EOL . ' ';
+print_r($dataAtual->format('d/m/Y H:i')) . PHP_EOL;
+
+$dataAtual2 = date('d/m/Y H:i');
+print_r($dataAtual2);
+
+$stringData = '2015-02-26';
+print_r(date('d/m/Y', strtotime($stringData))) . PHP_EOL;
