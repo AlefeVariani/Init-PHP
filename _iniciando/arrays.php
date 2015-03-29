5f852b1565c49328a8c59f6bbdb5d4b56fe2fd66
Alefe Variani <alefevariani18@gmail.com>
2015-03-28 22:24:01 -0300

    Ajustes

--- _iniciando/arrays.php
+++ _iniciando/arrays.php
@@ -1,16 +1,16 @@
 <?php
-/**
-* Referência  
-* @link http://php.net/manual/pt_BR/language.types.array.php 
-*/
 
+/**
+ * Referência  
+ * @link http://php.net/manual/pt_BR/language.types.array.php 
+ */
 $a[0] = "a";
 $a[1] = "b";
 
-echo $a;// Array
+echo $a; // Array
 
 $animais = array("Gato", "Cachorro", "Coelho");
-echo $animais[0];// Gato
+echo $animais[0]; // Gato
 
 $nomes = array(0 => "Alefe", 1 => "Joao"); //indices podem ser informados
 echo $nomes[0];
@@ -21,19 +21,19 @@ $conf["nome"] = "Alefe";
 $conf["idade"] = "20";
 $conf["linguagem"] = "PHP";
 
-echo "Nome - " . $conf["nome"] . " Idade - " . $conf["idadde"]; 
+echo "Nome - " . $conf["nome"] . " Idade - " . $conf["idadde"];
 
 $b = array(
-	'times' => array(
-		0 => 'gremio',
-		1 => 'chapecoense',
-		2 => 'boca junior'
-	),
-	'selecoes' => array(
-		'brasil',
-		'alemanha',
-		'argentina'
-	)
+    'times' => array(
+        0 => 'gremio',
+        1 => 'chapecoense',
+        2 => 'boca junior'
+    ),
+    'selecoes' => array(
+        'brasil',
+        'alemanha',
+        'argentina'
+    )
 );
 
 #Obs.: Atualmento é recomendao a declaração de array assim []
@@ -41,7 +41,7 @@ $b = array(
 $array = [];
 
 $array2 = [
-	'time' => [
-		'chape'
-	]
+    'time' => [
+        'chape'
+    ]
 ];
--- _iniciando/exibir_valores.php
+++ _iniciando/exibir_valores.php
@@ -18,12 +18,12 @@ echo PI;
 // alem de exiber, pode retornar um resultado, função do PHP
 print $nome;
 
-print_r($nome);
- 
+print_r($nome . PHP_EOL); // PHP_EOL quebra de linha no terminal "\n"
+
 var_dump($nome); 
 
 ?>
 
-<h1>Oi <?php echo $nome; ?></h1>
+<h1>Oi <?php echo $nome ?></h1>
 <!--Outra maneira -->
-<h1>Oi <?=$nome?></h1>
+<h1>Oi <?= $nome ?></h1>
