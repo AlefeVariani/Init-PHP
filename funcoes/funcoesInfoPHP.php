db1a6359caa1c8f163d4bc5734f90eb38a7e6d69
Alefe Variani <alefevariani18@gmail.com>
2015-07-07 16:20:36 -0300

    Renomeação dos arquivos funções e novos exemplo para funções com strings, arrays e informações do PHP

--- funcoes/funcoesInfoPHP.php
+++ funcoes/funcoesInfoPHP.php
@@ -9,3 +9,8 @@ echo zend_version() . PHP_EOL; // Versão Zend engine que esta rodando
 echo phpinfo() . PHP_EOL; // Informaçõe sobre o php instalado
 
 echo phpversion() . PHP_EOL; // Versão do PHP instalado
+
+$arr = get_defined_functions(); // Informa todas as funções do PHP existentes
+
+echo '<pre>';
+print_r($arr);
--- funcoes/funcoesParaArrays.php
+++ funcoes/funcoesParaArrays.php
@@ -7,14 +7,20 @@
 $arrayAlfabeto = array("a","b","c","e","f","g","h","i","j");
 print_r(implode(',', $arrayAlfabeto)); // Transforma array em uma string, pelo seu caracte informado
 
+$array1 = ['alefe', 'variani'];
+array_push($array1, '21 anos'); // Adiciona um elemento no array
+print_r($array1);
+
+$array2 = ['brasil', 'argentina', 'colombia'];
+array_pop($array2); // Retira a ultima posição do array
+array_shift($array2); // Retira a primeira posição do array
+
+$array3 = [1,2,3,4,5,6,7,8,9];
+echo array_sum($array3); // Soma os elementos do array
+
 $a = array_fill(1,5,'teste'); // Cria um array com 5 posições teste
 print_r($a) . PHP_EOL;
 
 $num = range(1,5);
 shuffle($num); // Mistura posições do array
 print_r($num) . PHP_EOL;
-
-$arr = get_defined_functions(); // Informa todas as funções do PHP existentes
-
-echo '<pre>';
-print_r($arr);
